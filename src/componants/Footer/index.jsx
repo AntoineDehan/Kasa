98817e6f2197c9fb859e52b89ae3eeeefb13192c
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2024-06-04 16:16:36 +0200

    Mise en place des Rootes et fichiers de bases.

---
+++ src/componants/Footer/index.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <nav>
+            <img src="" alt="" />
+            <p>© 2024 Kasa. All rights reserverd</p>
+        </nav>
+    )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
 import React from "react"
 import	ReactDOM from "react-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
+import About from "./pages/About"
+import Header from "./componants/Header"
+import Footer from "./componants/Footer"
+import Error from "./componants/Error"
 
 ReactDOM.render(
     <React.StrictMode>
-        <Home></Home>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/fiche-logement" element={<$ />} /> */}
+                <Route path="/a-propos" element={<About />} />
+                <Route path="/*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 )
